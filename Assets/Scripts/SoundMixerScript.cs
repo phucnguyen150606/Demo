ce4edf57cff360e388cb4c48df653edd3f77881a
phucnguyen150606 <phucnguyen150606@gmail.com>
2025-07-17 13:59:46 +0700

    Volume_Setting

--- Assets/Scripts/SoundMixerScript.cs
+++ Assets/Scripts/SoundMixerScript.cs
@@ -1,23 +1,65 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundMixerScript : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider masterVolumeSlider;
+    [SerializeField] private Slider SFXVolumeSlider;
+    //[SerializeField] private Slider MusicVolumeSlider;
 
-    public void SetMaterVolume(float Level)
+    public static SoundMixerScript instance;
+
+    private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        if (PlayerPrefs.HasKey("Master"))
+        {
+            LoadVolumeSettings();
+        }
+        else
+        {
+            SetMasterVolume();
+            SetSoundFXVolume();
+        }
+    }
+
+
+    public void SetMasterVolume()
+    {
+        float Level = masterVolumeSlider.value;
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(Level) * 20f);
+        PlayerPrefs.SetFloat("Master", Level);
     }
 
-    public void SetSoundFXVloume(float Level)
+    public void SetSoundFXVolume()
     {
+        float Level = SFXVolumeSlider.value;
         audioMixer.SetFloat("SoundFXVolume", Mathf.Log10(Level) * 20f);
+        PlayerPrefs.SetFloat("SoundFX", Level);
 
     }
 
-    public void SetMusicVolume(float Level)
+    //public void SetMusicVolume()
+    //{
+    //    float Level = MusicVolumeSlider.value;
+
+    //    audioMixer.SetFloat("MusicVolume", Mathf.Log10(Level) * 20f);
+    //    PlayerPrefs.SetFloat("MusicVolume", Level);
+    //}
+
+    public void LoadVolumeSettings()
     {
-        audioMixer.SetFloat("MusicVolume", Mathf.Log10(Level) * 20f);
+        masterVolumeSlider.value = PlayerPrefs.GetFloat("Master");
+        SFXVolumeSlider.value = PlayerPrefs.GetFloat("SoundFX");
+
+        SetMasterVolume();
+        SetSoundFXVolume();
+
     }
 }
